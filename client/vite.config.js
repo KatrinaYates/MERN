e10ac883e29bd5b216d58e878c13d66ba6c1d50c
Katrina Yates <katrina.yates@disney.com>
2026-05-10 15:51:52 -0400

    Connect frontend to backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://mern-6w4s.onrender.com/',
         changeOrigin: true,
       },
       '/images': {
-        target: 'http://localhost:3000',
+        target: 'https://mern-6w4s.onrender.com/',
         changeOrigin: true,
       },
     },
